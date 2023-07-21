643150fe07d8ee1d645f0d130f726eedee8fccad
Dexter <sohambasak42@gmail.com>
2023-07-21 22:38:03 +0530

    user route protected with JWT

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,10 +6,12 @@ const {
     getMe
 } = require('../controllers/userControl');
 
+const { protect } = require('../middleware/authMiddleware');
+
 router.post('/', registerUser);
 
 router.post('/login', loginUser);
 
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router;
