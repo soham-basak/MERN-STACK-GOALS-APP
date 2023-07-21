54979f664967352105910c5a0689c7f8b27cd3d5
Dexter <sohambasak42@gmail.com>
2023-07-21 20:07:10 +0530

    login function added

--- backend/controllers/userControl.js
+++ backend/controllers/userControl.js
@@ -43,15 +43,29 @@ const registerUser = asyncHandler (async (req, res) => {
         res.status(400);
         throw new Error('Something went wrong');
     }
-
-    res.json({message: 'register user'});
 })
 
 // @desc Authenticate a user
 // @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler (async (req, res) => {
-    res.json({message: 'login user'});
+
+    const { email, password } = req.body;
+
+    // Check for user email
+    const user = await User.findOne({email});
+
+    if(user && (await bcrypt.compare(password, user.password))){     
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+         
+    }else{
+            res.status(400);
+            throw new Error('Something went wrong');
+        }
 })
 
 // @desc Get user data
