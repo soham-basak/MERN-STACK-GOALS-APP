923ba2eb0fc7417df8871798f5fe97957e245d3b
Dexter <sohambasak42@gmail.com>
2023-07-23 16:11:02 +0530

    feat: login page template added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,68 @@
-const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
 
-export default Login
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSumbit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt />
+          Login
+        </h1>
+        <p>Login and start setting goals </p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSumbit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
